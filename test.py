ca9d683de180aa8d926f17b0e37c4ebf14935b00
RogueRedHat <103406560+RogueRedHat@users.noreply.github.com>
2024-02-10 19:59:17 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,1 @@
-import socket			
-print('This only tests if port 22 is open\n')
-s = socket.socket()		 
-try:
-  port = 22	
-  s.connect(('127.0.0.1', port))
-except:
-    print('port closed')
-print (s.recv(1024).decode())
-# close the connection 
-s.close()	 
-	
 
